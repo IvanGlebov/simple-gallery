d2010fb36633cae69a5a7917c319f155779e7955
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-04 18:10:02 +0300

    Added methods for fetching data, calculating number of images and manipulating with modal window

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,46 @@ function App() {
     let allImages = useAppSelector(getImages)
     let currentAlbumSize = !ignoreAlbumFilter ? allImages.length : allImages.filter((image) => image.albumId === album).length
     let images = allImages.filter(item => !ignoreAlbumFilter || item.albumId  === album).filter((image, index) => ((page-1)*pageSize <= index && index < page*pageSize))
+
+    let findPageSize = () => {
+        if (galleryRef.current !== null) {
+            let width = Math.floor(window.innerWidth * 0.8 / 180)
+            let height = Math.floor(window.innerHeight * 0.68 / 180)
+            setPageSize(height*width)
+        }
+    }
+
+    let openModal = (image: imageState) => {
+        setSelectedImage(image)
+        setTimeout(() => toggleModal(true), 100)
+    }
+
+    let hideModal = (e: any) => {
+        e.preventDefault()
+        if(modalRef.current !== null) {
+            if (!modalRef.current.contains(e.target) && !modalStatus) {
+                toggleModal(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        dispatch(fetchImagesAsync())
+        findPageSize()
+        window.addEventListener('click', hideModal)
+        return function cleanup () {
+            window.removeEventListener('click', hideModal)
+        }
+    }, [])
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => setLoading(false), 200)
+    }, [album, page, pageSize])
+
+    useEffect(() => {
+        setPage(1)
+    }, [album, ignoreAlbumFilter])
 }
 
 export default App;
